272139af43f5f7a01359317ccb014cb05f630541
sgm1 <sgmathai@att.net>
2014-02-15 11:36:01 -0600

    KeyHandler file

---
+++ Proj2_CS342/src/launcher/KeyHandler.java
@@ -0,0 +1,53 @@
+package launcher;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class KeyHandler extends MouseAdapter {
+	private boolean isDwn[] = new boolean[2];// to use for two mouse press together
+	private boolean hasClicked[] = new boolean[2];// to be reset at check
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {//combination of press release
+		if (e.getButton() == MouseEvent.BUTTON1){
+			System.out.println("Left, clicked");
+		}
+		if (e.getButton() == MouseEvent.BUTTON3){
+			System.out.println("Right, clicked");
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {//combination of press release
+		if (e.getButton() == MouseEvent.BUTTON1){
+			System.out.println("Left, pressed");
+		}
+		if (e.getButton() == MouseEvent.BUTTON3){
+			System.out.println("Right, pressed");
+			isDwn[0] = true;
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {//combination of press release
+		if (e.getButton() == MouseEvent.BUTTON1){
+			System.out.println("Left, released");
+		}
+		if (e.getButton() == MouseEvent.BUTTON3){
+			System.out.println("Right, released");
+			isDwn[0] = false;
+			hasClicked[0] = true;
+		}
+	}
+	
+	/**
+	 * Returns true if both are down
+	 * 
+	 * @return True if both are down
+	 */
+	public boolean isBothDown(){
+		return isDwn[0] && isDwn[1];
+	}
+
+}
