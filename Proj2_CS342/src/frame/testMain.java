90b0895ef6024cff136087d7b530355544466300
sgm1 <sgmathai@att.net>
2014-02-14 19:53:19 -0600

    fixed the auto-sizing with the custom layout for the minesweeper board

--- Proj2_CS342/src/frame/testMain.java
+++ Proj2_CS342/src/frame/testMain.java
@@ -11,6 +11,7 @@ public class testMain extends JFrame implements ActionListener{
 	
 	public static void main(String args[]){
 		testMain temp = new testMain();
+		temp.pack();
 		temp.setVisible(true);
 	}
 	
@@ -20,16 +21,16 @@ public class testMain extends JFrame implements ActionListener{
 		JMenuBar temp = new JMenuBar();
 		JMenu menu = new JMenu("FirstMenu");
 		temp.add(menu);
-		menu.add(new JMenuItem("A text-only menu item",
-                         KeyEvent.VK_T));
+		menu.add(new JMenuItem("A text-only menu item", KeyEvent.VK_T));
 		this.add(temp, BorderLayout.PAGE_START);
 		JPanel msBoard = new JPanel();
 		msBoard.setLayout(null);//for custom layout
 		JButton aButton;
-		int buttonSize = 30, numBlocks, sideCount;
+		final int buttonSize = 30, numBlocks, sideCount;
 		numBlocks = 100;
 		sideCount = (int)(Math.sqrt(numBlocks) + 0.001);//in case of weird round off error
-		msBoard.setMinimumSize(new Dimension(buttonSize * sideCount, buttonSize * sideCount));
+		msBoard.setPreferredSize((new Dimension((buttonSize + 1) * sideCount + 1, (buttonSize + 1) * sideCount + 1)));
+		System.out.println(msBoard.getMinimumSize());
 		msBoard.setBounds(0, 0, buttonSize * sideCount, buttonSize * sideCount);
 		for(int i = 0; i < numBlocks; i++){
 			aButton = new JButton("" + i);
@@ -49,7 +50,8 @@ public class testMain extends JFrame implements ActionListener{
 		Insets ins = this.getInsets();
 		System.out.println(ins);
 		//this.setSize(ins.left + ins.right, ins.top + ins.bottom);
-		this.setSize(sideCount * (buttonSize + 3), (sideCount + 1) * (buttonSize + 3));
+		//this.setSize(sideCount * (buttonSize + 3), (sideCount + 1) * (buttonSize + 3));
+		//this.pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//do a system.exit at end, i believe
 	}
 
